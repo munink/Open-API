c6b0521efe1b1d412de6e0e4c72c80717e887692
munink <kmunin987@gmail.com>
2023-06-13 23:10:30 +0530

    Open-API

---
+++ microservices-openapi/src/main/java/com/microservices/openapi/microservicesopenapi/MicroservicesOpenapiApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.openapi.microservicesopenapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesOpenapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesOpenapiApplication.class, args);
+	}
+
+}
---
+++ microservices-openapi/src/test/java/com/microservices/openapi/microservicesopenapi/MicroservicesOpenapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.openapi.microservicesopenapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesOpenapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
